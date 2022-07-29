19b0f8701816552aa02eeece7734d8cd4e8d0fed
Rio Jane Baldomer <riojane.baldomer@getpantheon.com>
2022-07-29 11:35:13 +0000

    Create freePlugins using scaffold

---
+++ wp-content/plugins/freePlugin/freePlugin.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     FreePlugin
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     freePlugin
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         FreePlugin
+ */
+
+// Your code starts here.
